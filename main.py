c1789b8e132b94ca6268651de7fa4bca54a7fad9
PimLeerkes <83347609+PimLeerkes@users.noreply.github.com>
2022-09-18 16:30:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import itertools
 
+#decrypts the ciphertext with a certain key:
 def decrypt(ciphertext, key):
     key_length = len(key)
     key_as_int = [ord(i) for i in key]
     ciphertext_int = [ord(i) for i in ciphertext]
     plaintext = ''
     for i in range(len(ciphertext_int)):
-        value = (ciphertext_int[i] - key_as_int[i % key_length])
-        plaintext += chr(value + 65)
-        print(value + 65)
+        value = (ciphertext_int[i] - key_as_int[i % key_length]) % 26
+        plaintext += chr(value + 97)
     return plaintext
 
 
-def sensible(plaintext):
-    if "THE" in plaintext:
-        return True
+#checks if plaintext makes sense:
+def sensible(plaintext, dictionary):
+    if "the" in plaintext:
+        count = 0
+        for word in dictionary:
+            if word in plaintext:
+                count = count + 1
+        if count > 2:
+            return True
 
 
 if __name__ == "__main__":
@@ -23,10 +29,18 @@ if __name__ == "__main__":
     symbols = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u','v', 'w', 'x', 'y', 'z']
     keys = itertools.permutations(symbols, keylength)
 
-    for key in keys:
-        plaintext = decrypt(ciphertext, "STING")
-        print(plaintext)
-        if sensible(plaintext):
-           print(plaintext)
+    #creating list of possible words to be in the plaintext:
+    dictionary = open("words.txt", "r")
+    dictionary = dictionary.readlines()
+    res = []
+    for sub in dictionary:
+        word = sub.replace("\n", "")
+        if len(word) > 4:
+            res.append(word)
 
+    #iterating over all keys:
+    for key in keys:
+        plaintext = decrypt(ciphertext, key)
+        if sensible(plaintext, res):
+           print("plaintext: " + plaintext + ", key: " + str(key))
 
